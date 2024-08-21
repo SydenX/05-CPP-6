5ab6efa30a75010f573e75a60243c344b6d9fbee
Jeremy Tollenaers <jtollena@fu-r4-p3.s19.be>
2024-08-21 12:24:50 +0200

    save

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -2,6 +2,7 @@
 #include "ScalarConverter.hpp"
 #include <sstream>
 #include <cmath>
+#include <iomanip>
 
 //Constructor
 ScalarConverter::ScalarConverter() {
@@ -22,78 +23,58 @@ ScalarConverter&	ScalarConverter::operator=(ScalarConverter const &scalarconvert
 	return *this;
 }
 
-std::string getChar(std::string &s){
-	std::string str;
-	double i = std::strtol(s.c_str(), NULL, 10);
-	if (s == "nan" || s == "+inf" || s == "-inf" || s == "+inff" || s == "-inff")
-		str = "impossible";
-	else if (i < 32 || i > 126)
-		str = "Not displayable";
+void printChar(double d){
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "char: ";
+	int i = static_cast<int>(std::round(d));
+	 if (i < 32 || i > 126)
+		std::cout << "Not displayable" << std::endl;
 	else {
-		str += '\'';
-		str += i;
-		str += '\'';
+		std::cout << '\'' << static_cast<char>(i) << '\'' << std::endl;
 	}
-	return str;
 }
 
-std::string getInt(std::string &s){
-	std::string str;
-	double d = std::strtol(s.c_str(), NULL, 10);
-	if (s == "nan" || s == "nanf" || s == "+inf" || s == "-inf" || s == "+inff" || s == "-inff")
-		str = "impossible";
-	else if (std::abs(d) > std::numeric_limits<int>::max())
-		str = "impossible";
+void printInt(double d){
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "int: ";
+	 if ((d) > std::numeric_limits<int>::max() || (d) < std::numeric_limits<int>::min())
+		std::cout << "impossible" << std::endl;
 	else {
-		std::ostringstream s; 
-		s << d; 
-		str = s.str();
+		int i = static_cast<int>(std::round(d));
+		std::cout << i << std::endl;
 	}
-	return str;
 }
 
-std::string getFloat(std::string &s){
-	std::string str;
-	if (s == "+inf" || s == "-inf" || s == "+inff" || s == "-inff")
-		return s;
-	double d = std::strtof(s.c_str(), NULL);
+void printFloat(double d){
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "float: ";
 	if (std::abs(d) > std::numeric_limits<float>::max())
-		str = "impossible";
+		std::cout << "impossible" << std::endl;
 	else {
-		std::ostringstream s; 
-		s << std::defaultfloat << d;
-		str += s.str();
-		if (!(str.find(".") != std::string::npos) && str != "nan")
-			str += ".0";
-		str += "f";
+		float f = static_cast<float>(d);
+		std::cout << f << "f" << std::endl;
 	}
-	return str;
 }
 
-std::string getDouble(std::string &s){
-	std::string str;
-	if (s == "+inf" || s == "-inf" || s == "+inff" || s == "-inff")
-		return s;
-	double d = std::strtod(s.c_str(), NULL);
+void printDouble(double d){
+	std::cout << std::fixed << std::setprecision(1);
+	std::cout << "double: ";
 	if (std::abs(d) > std::numeric_limits<double>::max())
-		str = "impossible";
+		std::cout << "impossible" << std::endl;
 	else {
-		std::ostringstream s; 
-		s << std::defaultfloat << d;
-		str = s.str();
-		if (!(str.find(".") != std::string::npos) && str != "nan")
-			str += ".0";
+		std::cout << d << std::endl;
 	}
-	return str;
 }
 
 int checkStr(std::string &str){
 	if (str == "nan" || str == "nanf" || str == "+inf" || str == "-inf" || str == "+inff" || str == "-inff")
-		return 0;
+		return -1;
 	if (str[0] < 32 || str[0] > 126)
 		return 1;
+	if (str[0] == '.')
+		return 8;
 	int hasPoint = 0;
-	for (int i = 1; i < (long long)str.length(); i++){
+	for (unsigned long i = 1; i < (unsigned long)str.length(); i++){
 		if (str[i - 1] < '0' || str[i - 1] > '9'){
 			if (str[i - 1] != '.' && str[i - 1] != '-')
 				return 2;
@@ -108,14 +89,14 @@ int checkStr(std::string &str){
 			return 6;
 		if (str[i] == '.')
 			hasPoint++;
-		if (hasPoint > 1)
+		if (hasPoint > 1 || (str[i] == '.' && i == str.length() - 1))
 			return 7;
 	}
 	return 0;
 }
 
 std::string charToInt(std::string in){
-	int i = in[0];
+	int i = static_cast<int>(in[0]);
 	std::ostringstream s; 
 	s << i;
 	return s.str();
@@ -123,13 +104,27 @@ std::string charToInt(std::string in){
 
 void ScalarConverter::convert(std::string in){
 	if (checkStr(in) != 0){
+		if (checkStr(in) == -1){
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			if (in == "nan" || in == "+inf" || in == "-inf"){
+				std::cout << "float: " << (in) << "f" << std::endl;
+				std::cout << "double: " << (in) << std::endl;
+			} else {
+				std::cout << "float: " << (in) << std::endl;
+				in[in.length() - 1] = 3;
+				std::cout << "double: " << (in) << std::endl;
+			}
+			return;
+		}
 		std::cout << "Argument not a comptatible literal." << std::endl;
 		return;
 	}
 	if (in.length() == 1 && in[0] >= 32 && in[0] <= 126 && (in[0] < '0' || in[0] > '9'))
 		in = charToInt(in);
-	std::cout << "char: " << getChar(in) << std::endl;
-	std::cout << "int: " << getInt(in) << std::endl;
-	std::cout << "float: " << getFloat(in) << std::endl;
-	std::cout << "double: " << getDouble(in) << std::endl;
+	double d = static_cast<double>(std::strtod(in.c_str(), NULL));
+	printChar(d);
+	printInt(d);
+	printFloat(d);
+	printDouble(d);
 };
